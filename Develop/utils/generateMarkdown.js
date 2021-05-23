b95936685d2918d1ad577f270f28e6ecbac0a24f
aw2389 <alex2@weghorst.com>
2021-05-23 01:13:09 -0500

    Finished badge section of generate Markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "https://img.shields.io/badge/License-MIT-yellow.svg";
+    case "APACHE 2.0":
+      return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+    case "GPL 3.0":
+      return "https://img.shields.io/badge/License-GPLv3-blue.svg";
+    case "BSD 3":
+      return "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg";
+    case "None":
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "APACHE 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL 3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "BSD 3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    case "None":
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseBadge = renderLicenseBadge(license);
+  let licenseLink = renderLicenseLink(license);
+
+  return `[![license](${licenseBadge})](${licenseLink})`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let licenseElement = renderLicenseSection(data.license);
   return `# ${data.title}
-  Licensing:
+  ${licenseElement}
 
   ## Description:
 
   ${data.description}
 
   ## Table of Contents:
-  
+
 * [Installation](#installation)
 * [Usage](#usage)
 * [License](#license)
@@ -45,7 +77,7 @@ function generateMarkdown(data) {
 
   ## Contributing:
 
-  ${data.contribution}
+  ${data.contributing}
 
   ## Tests:
 
